cb8bf295c97d22efcfe368b1025ce9391eb134db
hachi <eojin312@naver.com>
2020-08-22 23:28:48 +0900

    좋아요 기능 로그인 안하면 alert 창 띄우기

--- src/main/java/hachi/simpleboard/web/PostLikeApiController.java
+++ src/main/java/hachi/simpleboard/web/PostLikeApiController.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 @RestController
 @RequiredArgsConstructor
 @Slf4j
@@ -20,12 +24,16 @@ public class PostLikeApiController extends BaseApiController {
     private final PostLikeService postLikeService;
 
     @PostMapping("/like/{postId}")
-    public Long saveLike(@PathVariable("postId") Long postId, @AuthenticationPrincipal AuthUser authUser) {
+    public Long saveLike(@PathVariable("postId") Long postId, @AuthenticationPrincipal AuthUser authUser, HttpServletResponse response) throws IOException {
         long likeCount = 0;
         try {
             likeCount = postLikeService.save(postId, authUser.getUser());
         } catch (DataIntegrityViolationException dive) { // unique index제약에 걸렸을 경우 Exception처리 (이미 좋아요하셨어요~~)
             return -1L;
+        }
+        if (authUser == null) {
+            PrintWriter out = response.getWriter();
+            out.println("<script>if(confirm('로그인이 필요한 서비스입니다. 로그인 하시겠습니까?');){window.loacation.href='/login'} else{return;}</script>");
         }
         return likeCount;
     }
