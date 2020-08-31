c4410c8ec50fc08bc6c93e90b4b7ecd67aef8ec6
hachi <eojin312@naver.com>
2020-08-31 19:23:38 +0900

    카테고리를 파라미터로 최신글 순서로 정렬하는 메소드 추가

--- src/main/java/hachi/simpleboard/domain/post/PostRepository.java
+++ src/main/java/hachi/simpleboard/domain/post/PostRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 /**
  * 게시물 repository
  */
@@ -21,4 +23,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Modifying(clearAutomatically = true) // 됨
     @Query("update Post p set p.view = p.view + 1 where p.id = :id")
     int plusReadCount(@Param("id") Long id);
+
+    List<Post> findAllByCategoryOrderByIdDesc(String category);
 }
