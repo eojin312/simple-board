b449fe8919a3b56113978b362a0ef40add775399
hachi <eojin312@naver.com>
2020-09-06 15:28:18 +0900

    mockMvc 페이징, 검색어 테스트 작성 및 테스트 완료

--- src/test/java/hachi/simpleboard/web/PostsApiControllerTest.java
+++ src/test/java/hachi/simpleboard/web/PostsApiControllerTest.java
@@ -4,7 +4,7 @@ import hachi.simpleboard.BaseApiControllerTest;
 import org.junit.jupiter.api.Test;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -13,7 +13,27 @@ class PostsApiControllerTest extends BaseApiControllerTest {
     @Test
     void 리스트가_200OK() throws Exception {
         mockMvc.perform(get("/api/posts"))
-                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void 페이징_테스트() throws Exception {
+        mockMvc.perform(get("/api/posts").param("page", "1"))
+                .andExpect(jsonPath("$.page.totalElements").exists())
+                .andExpect(jsonPath("$.page.content[0].id").isNumber())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void 검색어_테스트() throws Exception {
+        mockMvc.perform(
+                get("/api/posts")
+                        .param("page", "1")
+                        .param("searchType", "title")
+                        .param("searchKeyword", "est")
+        )
+                .andExpect(jsonPath("$.page.totalElements").exists())
+                .andExpect(jsonPath("$.page.content[0].id").isNumber())
                 .andExpect(status().isOk());
     }
 }
