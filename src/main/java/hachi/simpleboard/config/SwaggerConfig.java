c64871f47e2c8241b6260b369b2cdf842a227222
hachi <eojin312@naver.com>
2020-11-03 15:55:59 +0900

    swagger despcription 추가

--- src/main/java/hachi/simpleboard/config/SwaggerConfig.java
+++ src/main/java/hachi/simpleboard/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 /**
  * Swagger UI 를 사용하기 위한 설정 클래스
@@ -27,6 +31,21 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/api/**"))
-                .build();
+                .build()
+                .apiInfo(apiInfo(title, version))
+                ;
+    }
+
+    private ApiInfo apiInfo(String title, String version) {
+        return new ApiInfo(
+                title,
+                "API Swagger",
+                version,
+                "V1",
+                new Contact("Contact Me", "localhost:8080", "eojin312@naver.com"),
+                "Lisence",
+                "localhost:8080",
+                new ArrayList<>()
+        );
     }
 }
